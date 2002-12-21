9bc04fbbec57574b8a7a77af255abed6128abcec
William Lachance <wrlach@gmail.com>
2002-12-21 03:11:01 +0000

    consolidated attribute class hierarchy

---
+++ src/lib/WP6AttributeGroup.cpp
@@ -0,0 +1,60 @@
+/* libwpd2
+ * Copyright (C) 2002 William Lachance (wlach@interlog.com)
+ * Copyright (C) 2002 Marc Maurer (j.m.maurer@student.utwente.nl)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * For further information visit http://libwpd.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by 
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+#include "WP6AttributeGroup.h"
+#include "WP6LLListener.h"
+
+WP6AttributeGroup::WP6AttributeGroup(FILE *stream)
+	: WP6FixedLengthGroup()
+{
+	_read(stream, WP6_ATTRIBUTE_GROUP_SIZE);
+}
+
+gboolean WP6AttributeGroup::_readContents(FILE *stream)
+{
+	WPD_CHECK_FILE_READ_ERROR(fread(&m_attribute, sizeof(guint8), 1, stream), 1);
+
+	return TRUE;
+}
+
+WP6AttributeOnGroup::WP6AttributeOnGroup(FILE *stream) : WP6AttributeGroup(stream)
+{
+}
+
+gboolean WP6AttributeOnGroup::parse(WP6LLListener *llListener)
+{
+	// FIXME: handle flags
+	llListener->attributeChange(TRUE, getAttribute());
+}
+
+WP6AttributeOffGroup::WP6AttributeOffGroup(FILE *stream) : WP6AttributeGroup(stream)
+{
+}
+
+gboolean WP6AttributeOffGroup::parse(WP6LLListener *llListener)
+{
+	// FIXME: handle flags
+	llListener->attributeChange(FALSE, getAttribute());
+}
---
+++ src/lib/WP6AttributeGroup.h
@@ -0,0 +1,60 @@
+/* libwpd2
+ * Copyright (C) 2002 William Lachance (wlach@interlog.com)
+ * Copyright (C) 2002 Marc Maurer (j.m.maurer@student.utwente.nl)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * For further information visit http://libwpd.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by 
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+#ifndef WP6ATTRIBUTEGROUP_H
+#define WP6ATTRIBUTEGROUP_H
+
+#include "WP6FixedLengthGroup.h"
+
+class WP6AttributeGroup : public WP6FixedLengthGroup
+{
+ public:
+	WP6AttributeGroup(FILE *stream);	
+	virtual gboolean parse(WP6LLListener *llListener) = 0;
+	const guint8 getAttribute() const { return m_attribute; }
+	
+ protected:
+	virtual gboolean _readContents(FILE *stream);
+
+ private:
+	guint8 m_attribute;
+};
+
+class WP6AttributeOnGroup : public WP6AttributeGroup
+{
+ public:
+	WP6AttributeOnGroup(FILE *stream);
+	virtual gboolean parse(WP6LLListener *llListener);
+};
+
+class WP6AttributeOffGroup : public WP6AttributeGroup
+{
+ public:
+	WP6AttributeOffGroup(FILE *stream);
+	virtual gboolean parse(WP6LLListener *llListener);
+};
+
+
+#endif /* WP6ATTRIBUTEGROUP_H */
