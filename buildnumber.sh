90078e735f4b842d6fc3bef58b46218d1836f899
Marc Maurer <uwog@uwog.net>
2004-01-31 19:05:16 +0000

    Windows versioning: add a BUILDNUMBER (Fridrich Strba)

---
+++ buildnumber.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Script to create the BUILDNUMBER used by compile-resource. This script
+# needs the script createBuildNumber.pl to be in the same directory.
+
+{ ./createBuildNumber.pl \
+	src/lib/libwpd-build.stamp \
+	src/conv/html/wpd2html-build.stamp \
+	src/conv/raw/wpd2raw-build.stamp \
+	src/conv/text/wpd2text-build.stamp
+#Success
+exit 0
+}
+#Unsucessful
+exit 1
