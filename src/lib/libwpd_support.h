71ab7dc132b72faed44ad37d2e4adfe5913c3608
Fridrich Strba <fridrich.strba@bluewin.ch>
2004-06-06 20:19:36 +0000

    Correction of a typo; MinGW has a stdint.h implemented

--- src/lib/libwpd_support.h
+++ src/lib/libwpd_support.h
@@ -27,13 +27,13 @@
 #define LIBWPD_SUPPORT_H
 #include <glib.h>
 
-#ifndef WIN32
+#ifndef _MSC_VER
 #include <stdint.h>
 #else
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
-typedef signed short int8_t;
-typedef unsigned short uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
 #endif
