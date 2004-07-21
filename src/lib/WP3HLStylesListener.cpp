9053e59b2b6a581e6511787446c9f2babc5e8c41
Marc Maurer <uwog@uwog.net>
2004-07-21 12:21:57 +0000

    Initial Mac WordPerfect 3.0-3.5 file format support

---
+++ src/lib/WP3HLStylesListener.cpp
@@ -0,0 +1,169 @@
+/* libwpd
+ * Copyright (C) 2004 Marc Maurer (j.m.maurer@student.utwente.nl)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * For further information visit http://libwpd.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by 
+ * Corel Corporation or Corel Corporation Limited."
+ */
+ 
+#include "WP3HLStylesListener.h"
+#include "WPXTable.h"
+//#include "WP3FileStructure.h"
+#include "WPXFileStructure.h"
+#include "libwpd_internal.h"
+
+WP3HLStylesListener::WP3HLStylesListener(vector<WPXPageSpan *> *pageList, WPXTableList *tableList) : 
+	WP3HLListener(pageList, NULL),
+	m_currentPage(new WPXPageSpan()),
+	m_tableList(tableList), 
+	m_currentPageHasContent(false),
+	m_tempMarginLeft(1.0f),
+	m_tempMarginRight(1.0f)
+{
+}
+
+void WP3HLStylesListener::endDocument()
+{	
+	insertBreak(WPX_SOFT_PAGE_BREAK); // pretend we just had a soft page break (for the last page)
+	delete(m_currentPage); // and delete the non-existent page that was allocated as a result (scandalous waste!)
+}
+
+void WP3HLStylesListener::insertBreak(const uint8_t breakType)
+{
+	//if (!isUndoOn())
+	//{	
+		switch (breakType) 
+		{
+		case WPX_PAGE_BREAK:
+		case WPX_SOFT_PAGE_BREAK:
+			if (WPXHLListener::m_pageList->size() > 0 && (*m_currentPage)==(*(m_pageList->back())))
+			{
+				int oldPageSpan = m_pageList->back()->getPageSpan();
+				m_pageList->back()->setPageSpan(oldPageSpan + 1);
+				delete(m_currentPage);
+			}
+			else
+			{
+				m_pageList->push_back(m_currentPage);
+			}
+			m_currentPage = new WPXPageSpan(*(m_pageList->back()));
+			m_currentPage->setMarginLeft(m_tempMarginLeft);
+			m_currentPage->setMarginRight(m_tempMarginRight);
+			m_currentPageHasContent = false;
+			break;
+		}
+	//}
+}
+/*
+void WP3HLStylesListener::pageMarginChange(const uint8_t side, const uint16_t margin)
+{
+	if (!isUndoOn()) 
+	{
+		float marginInch = (float)(((double)margin + (double)WP6_NUM_EXTRA_WPU) / (double)WPX_NUM_WPUS_PER_INCH);
+		switch(side)
+		{
+			case WP6_PAGE_GROUP_TOP_MARGIN_SET:
+				m_currentPage->setMarginTop(marginInch);
+				break;
+			case WP6_PAGE_GROUP_BOTTOM_MARGIN_SET:
+				m_currentPage->setMarginBottom(marginInch);
+				break;
+		}
+	}
+}
+
+void WP3HLStylesListener::marginChange(const uint8_t side, const uint16_t margin)
+{
+	if (!isUndoOn()) 
+	{		
+		float marginInch = (float)(((double)margin + (double)WP6_NUM_EXTRA_WPU) / (double)WPX_NUM_WPUS_PER_INCH);
+		switch(side)
+		{
+			case WP6_COLUMN_GROUP_LEFT_MARGIN_SET:
+				if (!m_currentPageHasContent)
+					m_currentPage->setMarginLeft(marginInch);
+				m_tempMarginLeft = marginInch;
+				break;
+			case WP6_COLUMN_GROUP_RIGHT_MARGIN_SET:
+				if (!m_currentPageHasContent)
+					m_currentPage->setMarginRight(marginInch);
+				m_tempMarginRight = marginInch;
+				break;
+		}
+		
+	}
+
+}
+
+void WP3HLStylesListener::headerFooterGroup(const uint8_t headerFooterType, const uint8_t occurenceBits, const uint16_t textPID)
+{
+	if (!isUndoOn()) 
+	{			
+		WPD_DEBUG_MSG(("WordPerfect: headerFooterGroup (headerFooterType: %i, occurenceBits: %i, textPID: %i)\n", 
+			       headerFooterType, occurenceBits, textPID));
+		if (headerFooterType <= WP6_HEADER_FOOTER_GROUP_FOOTER_B) // ignore watermarks for now
+			m_currentPage->setHeaderFooter(headerFooterType, occurenceBits, textPID);
+	}
+}
+
+void WP3HLStylesListener::suppressPageCharacteristics(const uint8_t suppressCode)
+{
+	if (!isUndoOn()) 
+	{			
+		WPD_DEBUG_MSG(("WordPerfect: suppressPageCharacteristics (suppressCode: %u)\n", suppressCode));
+		if (suppressCode & WP6_PAGE_GROUP_SUPPRESS_HEADER_A)
+			m_currentPage->setHeadFooterSuppression(WP6_HEADER_FOOTER_GROUP_HEADER_A, true);
+		if (suppressCode & WP6_PAGE_GROUP_SUPPRESS_HEADER_B)
+			m_currentPage->setHeadFooterSuppression(WP6_HEADER_FOOTER_GROUP_HEADER_B, true);
+		if (suppressCode & WP6_PAGE_GROUP_SUPPRESS_FOOTER_A)
+			m_currentPage->setHeadFooterSuppression(WP6_HEADER_FOOTER_GROUP_FOOTER_A, true);
+		if (suppressCode & WP6_PAGE_GROUP_SUPPRESS_FOOTER_B)
+			m_currentPage->setHeadFooterSuppression(WP6_HEADER_FOOTER_GROUP_FOOTER_B, true);			
+	}
+}
+*/
+void WP3HLStylesListener::startTable()
+{
+	//if (!isUndoOn()) 
+	//{			
+		m_currentPageHasContent = true;
+		m_currentTable = new WPXTable();
+		m_tableList->add(m_currentTable);
+	//}
+}
+
+void WP3HLStylesListener::insertRow()
+{
+	if (/*!isUndoOn() && */m_currentTable != NULL) 
+	{
+		m_currentPageHasContent = true;
+		m_currentTable->insertRow();
+	}
+}
+
+void WP3HLStylesListener::insertCell(const uint8_t colSpan, const uint8_t rowSpan, const bool boundFromLeft, const bool boundFromAbove, 
+				  const uint8_t borderBits, 
+				  const RGBSColor * cellFgColor, const RGBSColor * cellBgColor)
+{
+	if (/*!isUndoOn() &&*/ m_currentTable != NULL)
+	{
+		m_currentPageHasContent = true;
+		m_currentTable->insertCell(colSpan, rowSpan, boundFromLeft, boundFromAbove, borderBits);
+	}
+}
---
+++ src/lib/WP3HLStylesListener.h
@@ -0,0 +1,81 @@
+/* libwpd
+ * Copyright (C) 2004 Marc Maurer (j.m.maurer@student.utwente.nl)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * For further information visit http://libwpd.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+#ifndef WP3HLSTYLESLISTENER_H
+#define WP3HLSTYLESLISTENER_H
+
+#include "WP3HLListener.h"
+#include <vector>
+#include "WPXPageSpan.h"
+#include "WPXTable.h"
+
+class WP3HLStylesListener : public WP3HLListener
+{
+public:
+	WP3HLStylesListener(vector<WPXPageSpan *> *pageList, WPXTableList *tableList);
+
+	virtual void startDocument() {}
+	virtual void setAlignmentCharacter(const uint16_t character) {}
+	virtual void setLeaderCharacter(const uint16_t character, const uint8_t numberOfSpaces) {}
+	virtual void defineTabStops(const bool isRelative, const vector<WPXTabStop> &tabStops, 
+				    const vector<bool> &usePreWP9LeaderMethods) {}
+	virtual void insertCharacter(const uint16_t character) {}
+	virtual void insertTab(const uint8_t tabType, const uint16_t tabPosition) {}
+	virtual void insertEOL() {}
+ 	virtual void insertBreak(const uint8_t breakType);
+	virtual void attributeChange(const bool isOn, const uint8_t attribute) {}
+	virtual void lineSpacingChange(const float lineSpacing) {}
+	virtual void spacingAfterParagraphChange(const float spacingRelative, const float spacingAbsolute) {}
+	virtual void justificationChange(const uint8_t justification) {}
+	virtual void pageMarginChange(const uint8_t side, const uint16_t margin) {}
+	virtual void pageFormChange(const uint16_t length, const uint16_t width, const WPXFormOrientation orientation) {}
+	virtual void marginChange(const uint8_t side, const uint16_t margin) {}
+	virtual void paragraphMarginChange(const uint8_t side, const int16_t margin) {}
+	virtual void indentFirstLineChange(const int16_t offset) {}
+	virtual void columnChange(const uint8_t numColumns) {}
+	virtual void endDocument();
+
+	virtual void defineTable(uint8_t position, uint16_t leftOffset){}
+	virtual void addTableColumnDefinition(uint32_t width, uint32_t leftGutter, uint32_t rightGutter){}
+	virtual void startTable();
+ 	virtual void insertRow();
+ 	virtual void insertCell(const uint8_t colSpan, const uint8_t rowSpan, const bool boundFromLeft, const bool boundFromAbove,
+				const uint8_t borderBits,
+				const RGBSColor * cellFgColor, const RGBSColor * cellBgColor);
+ 	virtual void endTable() {}
+
+
+protected:
+	virtual void _openPageSpan() { /* FIXME: REMOVE ME WHEN IMPLEMENTED IN WPXHLListener */ };
+
+private:
+	WPXPageSpan *m_currentPage;
+
+	WPXTableList *m_tableList;
+	WPXTable *m_currentTable;
+	float m_tempMarginLeft, m_tempMarginRight;
+	bool m_currentPageHasContent;
+};
+
+#endif /* WP3HLSTYLESLISTENER_H */
