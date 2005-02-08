b8a48495dd110d824f577080747b366ee3e8be59
Fridrich Strba <fridrich.strba@bluewin.ch>
2005-02-08 19:01:12 +0000

    Fixes for Solaris SPARC build inside OOo

--- src/lib/libwpd_types.h
+++ src/lib/libwpd_types.h
@@ -1,9 +1,6 @@
 #ifndef LIBWPD_TYPES_H
 #define LIBWPD_TYPES_H
 
-#if defined (__GNUC__) || defined (GCC)
-#include <stdint.h>
-#endif
 #ifdef _MSC_VER
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
@@ -11,6 +8,8 @@ typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
-#endif
+#else /* _MSC_VER */
+#include <inttypes.h>
+#endif /* _MSC_VER */
 
 #endif /* LIBWPD_TYPES_H */
