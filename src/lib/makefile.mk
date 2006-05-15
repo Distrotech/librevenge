0e5534cbbad147b233e72ba5b94a0ce0990d493d
Fridrich Strba <fridrich.strba@bluewin.ch>
2006-05-15 19:00:45 +0000

    Updating the makefile.mk used inside OOo build.

--- src/lib/makefile.mk
+++ src/lib/makefile.mk
@@ -20,6 +20,7 @@ SLOFILES= \
 	$(SLO)$/libwpd_internal.obj \
 	$(SLO)$/libwpd_math.obj \
 	$(SLO)$/WP3AttributeGroup.obj \
+	$(SLO)$/WP3ContentListener.obj \
 	$(SLO)$/WP3DefinitionGroup.obj \
 	$(SLO)$/WP3DisplayGroup.obj \
 	$(SLO)$/WP3EndOfLinePageGroup.obj \
@@ -29,6 +30,7 @@ SLOFILES= \
 	$(SLO)$/WP3FontGroup.obj \
 	$(SLO)$/WP3FootnoteEndnoteGroup.obj \
 	$(SLO)$/WP3Header.obj \
+	$(SLO)$/WP3HeaderFooterGroup.obj \
 	$(SLO)$/WP3Listener.obj \
 	$(SLO)$/WP3MiscellaneousGroup.obj \
 	$(SLO)$/WP3PageFormatGroup.obj \
@@ -42,6 +44,7 @@ SLOFILES= \
 	$(SLO)$/WP3UnsupportedFixedLengthGroup.obj \
 	$(SLO)$/WP3UnsupportedVariableLengthGroup.obj \
 	$(SLO)$/WP3VariableLengthGroup.obj \
+	$(SLO)$/WP42ContentListener.obj \
 	$(SLO)$/WP42FileStructure.obj \
 	$(SLO)$/WP42Heuristics.obj \
 	$(SLO)$/WP42Listener.obj \
@@ -51,25 +54,33 @@ SLOFILES= \
 	$(SLO)$/WP42UnsupportedVariableLengthGroup.obj \
 	$(SLO)$/WP42VariableLengthGroup.obj \
 	$(SLO)$/WP5AttributeGroup.obj \
+	$(SLO)$/WP5ContentListener.obj \
 	$(SLO)$/WP5DefinitionGroup.obj \
 	$(SLO)$/WP5ExtendedCharacterGroup.obj \
 	$(SLO)$/WP5FileStructure.obj \
 	$(SLO)$/WP5FixedLengthGroup.obj \
-	$(SLO)$/WP5FormatGroup.obj \
+	$(SLO)$/WP5FontGroup.obj \
+	$(SLO)$/WP5FontNameStringPoolPacket.obj \
+	$(SLO)$/WP5FootnoteEndnoteGroup.obj \
+	$(SLO)$/WP5GeneralPacketData.obj \
+	$(SLO)$/WP5GeneralPacketIndex.obj \
 	$(SLO)$/WP5Header.obj \
+	$(SLO)$/WP5HeaderFooterGroup.obj \
 	$(SLO)$/WP5Listener.obj \
+	$(SLO)$/WP5ListFontsUsedPacket.obj \
 	$(SLO)$/WP5PageFormatGroup.obj \
 	$(SLO)$/WP5Parser.obj \
 	$(SLO)$/WP5Part.obj \
+	$(SLO)$/WP5PrefixData.obj \
 	$(SLO)$/WP5SingleByteFunction.obj \
+	$(SLO)$/WP5SpecialHeaderIndex.obj \
 	$(SLO)$/WP5StylesListener.obj \
+	$(SLO)$/WP5SubDocument.obj \
 	$(SLO)$/WP5TableEOLGroup.obj \
 	$(SLO)$/WP5TableEOPGroup.obj \
 	$(SLO)$/WP5UnsupportedFixedLengthGroup.obj \
 	$(SLO)$/WP5UnsupportedVariableLengthGroup.obj \
 	$(SLO)$/WP5VariableLengthGroup.obj \
-	$(SLO)$/WP60Header.obj \
-	$(SLO)$/WP61Header.obj \
 	$(SLO)$/WP6AttributeGroup.obj \
 	$(SLO)$/WP6CharacterGroup.obj \
 	$(SLO)$/WP6ColumnGroup.obj \
@@ -86,17 +97,19 @@ SLOFILES= \
 	$(SLO)$/WP6FootnoteEndnoteGroup.obj \
 	$(SLO)$/WP6GeneralTextPacket.obj \
 	$(SLO)$/WP6Header.obj \
+	$(SLO)$/WP60Header.obj \
+	$(SLO)$/WP61Header.obj \
 	$(SLO)$/WP6HeaderFooterGroup.obj \
 	$(SLO)$/WP6HighlightGroup.obj \
 	$(SLO)$/WP6Listener.obj \
+	$(SLO)$/WP6Parser.obj \
 	$(SLO)$/WP6OutlineStylePacket.obj \
 	$(SLO)$/WP6PageGroup.obj \
 	$(SLO)$/WP6ParagraphGroup.obj \
-	$(SLO)$/WP6Parser.obj \
 	$(SLO)$/WP6Part.obj \
 	$(SLO)$/WP6PrefixData.obj \
-	$(SLO)$/WP6PrefixDataPacket.obj \
 	$(SLO)$/WP6PrefixIndice.obj \
+	$(SLO)$/WP6PrefixDataPacket.obj \
 	$(SLO)$/WP6SingleByteFunction.obj \
 	$(SLO)$/WP6StyleGroup.obj \
 	$(SLO)$/WP6StylesListener.obj \
@@ -106,19 +119,19 @@ SLOFILES= \
 	$(SLO)$/WP6UnsupportedFixedLengthGroup.obj \
 	$(SLO)$/WP6UnsupportedVariableLengthGroup.obj \
 	$(SLO)$/WP6VariableLengthGroup.obj \
-	$(SLO)$/WPDocument.obj \
+	$(SLO)$/WPXPageSpan.obj \
 	$(SLO)$/WPXHeader.obj \
 	$(SLO)$/WPXListener.obj \
 	$(SLO)$/WPXMemoryStream.obj \
-	$(SLO)$/WPXPageSpan.obj \
+	$(SLO)$/WPXTable.obj \
 	$(SLO)$/WPXParser.obj \
 	$(SLO)$/WPXPart.obj \
 	$(SLO)$/WPXProperty.obj \
 	$(SLO)$/WPXPropertyList.obj \
-	$(SLO)$/WPXPropertyListVector.obj \
 	$(SLO)$/WPXString.obj \
 	$(SLO)$/WPXSubDocument.obj \
-	$(SLO)$/WPXTable.obj
+	$(SLO)$/WPXPropertyListVector.obj \
+	$(SLO)$/WPDocument.obj
 
 LIB1ARCHIV=$(LB)$/libwpdlib.a
 LIB1TARGET=$(SLB)$/$(TARGET).lib
