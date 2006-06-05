fac1dfa07d023bb6f7939b9ad3d2724bee8ecd0b
Fridrich Strba <fridrich.strba@bluewin.ch>
2006-06-05 19:29:57 +0000

    WP42SubDocument class in view of header/footer footnote/endnote support in WP42 parser

---
+++ src/lib/WP42SubDocument.cpp
@@ -0,0 +1,39 @@
+/* libwpd
+ * Copyright (C) 2006 Fridrich Strba (fridrich.strba@bluewin.ch)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * For further information visit http://libwpd.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by 
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+#include "WP42SubDocument.h"
+#include "WP42Parser.h"
+#include "libwpd_internal.h"
+
+WP42SubDocument::WP42SubDocument(uint8_t * streamData, const int dataSize) :
+	WPXSubDocument(streamData, dataSize)
+{
+}
+
+void WP42SubDocument::parse(WP42Listener *listener) const
+{
+	WPXMemoryInputStream *tmpStream = getStream();
+	tmpStream->seek(0, WPX_SEEK_SET);
+	WP42Parser::parseDocument(tmpStream, listener);
+}
