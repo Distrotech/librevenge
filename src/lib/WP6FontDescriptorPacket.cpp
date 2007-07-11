aa7915eefbc0efb1c73e6e25b16e11184f4b50e9
Fridrich Strba <fridrich.strba@bluewin.ch>
2007-07-11 13:51:59 +0000

    some warnings about unused variables

--- src/lib/WP6FontDescriptorPacket.cpp
+++ src/lib/WP6FontDescriptorPacket.cpp
@@ -26,6 +26,7 @@
 #include <limits>
 #include "WP6FontDescriptorPacket.h"
 #include "libwpd_internal.h"
+#include <string>
 
 const char * FONT_WEIGHT_STRINGS [] = {	"Bold", "bold", "Demi", "demi", "Extended", "extended",
 					"Extra", "extra", "Headline", "headline", "Light", "light",
@@ -65,89 +66,77 @@ WP6FontDescriptorPacket::~WP6FontDescriptorPacket()
 
 void WP6FontDescriptorPacket::_readContents(WPXInputStream *input)
 {
-   // short sized characteristics
-   m_characterWidth = readU16(input);
-   m_ascenderHeight = readU16(input);
-   m_xHeight = readU16(input);
-   m_descenderHeight = readU16(input);
-   m_italicsAdjust = readU16(input);
-   // byte sized characteristics
-   m_primaryFamilyMemberId = readU8(input);
-   m_primaryFamilyId = readU8(input);
-   m_scriptingSystem = readU8(input);
-   m_primaryCharacterSet = readU8(input);
-   m_width = readU8(input);
-   m_weight = readU8(input);
-   m_attributes = readU8(input);
-   m_generalCharacteristics = readU8(input);
-   m_classification = readU8(input);
-   m_fill = readU8(input);
-   m_fontType = readU8(input);
-   m_fontSourceFileType = readU8(input);
+	// short sized characteristics
+	m_characterWidth = readU16(input);
+	m_ascenderHeight = readU16(input);
+	m_xHeight = readU16(input);
+	m_descenderHeight = readU16(input);
+	m_italicsAdjust = readU16(input);
+	// byte sized characteristics
+	m_primaryFamilyMemberId = readU8(input);
+	m_primaryFamilyId = readU8(input);
+	m_scriptingSystem = readU8(input);
+	m_primaryCharacterSet = readU8(input);
+	m_width = readU8(input);
+	m_weight = readU8(input);
+	m_attributes = readU8(input);
+	m_generalCharacteristics = readU8(input);
+	m_classification = readU8(input);
+	m_fill = readU8(input);
+	m_fontType = readU8(input);
+	m_fontSourceFileType = readU8(input);
 
-   m_fontNameLength = readU16(input); 
+	m_fontNameLength = readU16(input); 
 
 
-   if (m_fontNameLength > ((std::numeric_limits<uint16_t>::max)() / 2))
+	if (m_fontNameLength > ((std::numeric_limits<uint16_t>::max)() / 2))
 	m_fontNameLength = ((std::numeric_limits<uint16_t>::max)() / 2);
-   if (m_fontNameLength) 
-	   {
-		   uint16_t tempLength=0;
-		   int numTokens=0;
-		   int lastTokenPosition=0;
-		   for (uint16_t i=0; i<(m_fontNameLength/2); i++) 
-		   {
-			   uint16_t charWord = readU16(input);
+	if (m_fontNameLength) 
+	{
+		for (uint16_t i=0; i<(m_fontNameLength/2); i++) 
+		{
+			uint16_t charWord = readU16(input);
+			uint8_t characterSet = (uint8_t)((charWord >> 8) & 0x00FF);
+			uint8_t character = (uint8_t)(charWord & 0xFF);
 
-			   uint8_t characterSet = (uint8_t)((charWord >> 8) & 0x00FF);
-			   uint8_t character = (uint8_t)(charWord & 0xFF);
-			   if (character == 0x00 && characterSet == 0x00)
-			   	break;
-			   const uint16_t *chars;
-			   int len = extendedCharacterWP6ToUCS2(character, characterSet, &chars);
-			   /* We are only using ascii characters here, and
-			    * if we have more than one character, that's
-			    * going to be an international character, so
-			    * we don't actually iterate through the list of
-			    * characters returned - we assume that just one
-			    * character was returned.
-			    */
+			if (character == 0x00 && characterSet == 0x00)
+				break;
 
-			   for (int j = 0; j < len; j++)
-			   	appendUCS4(m_fontName, (uint32_t)chars[j]);
-		   }
+			const uint16_t *chars;
+			int len = extendedCharacterWP6ToUCS2(character, characterSet, &chars);
 
-		   WPD_DEBUG_MSG(("WordPerfect: stripping font name (original: %s)\n", m_fontName.cstr()));
-		   std::string stringValue(m_fontName.cstr());
-		   std::string::size_type pos;
-		   for (unsigned k = 0; k < countElements(FONT_WEIGHT_STRINGS); k++)
-		   {
-		   	if (!stringValue.empty())
-    				while ((pos = stringValue.find(FONT_WEIGHT_STRINGS[k])) != std::string::npos)
-          				stringValue.replace(pos, strlen(FONT_WEIGHT_STRINGS[k]),"");
-		   }
-		   
-		   // SPECIAL CASE: eliminate the -WP postfix (if it's there), which isn't spaced out from
-		   // the rest of the font
-		   if (!stringValue.empty())
-		   	while ((pos = stringValue.find(USELESS_WP_POSTFIX)) != std::string::npos)
-				stringValue.replace(pos, strlen(USELESS_WP_POSTFIX), "");
+			for (int j = 0; j < len; j++)
+				appendUCS4(m_fontName, (uint32_t)chars[j]);
+		}
 
-		   // also consume any whitespace at the end of the font.
-		   if (!stringValue.empty())
-		   	while ((pos = stringValue.find("  ")) != std::string::npos)
-				stringValue.replace(pos, strlen("  "), " ");
-		   if (!stringValue.empty())
-		   	while ((pos = stringValue.find(" ", stringValue.size() - 1)) != std::string::npos)
-				stringValue.replace(pos, strlen(" "), "");
-		   if (!stringValue.empty())
-		   	while ((pos = stringValue.find("-", stringValue.size() - 1)) != std::string::npos)
-				stringValue.replace(pos, strlen("-"), "");
-		
-		   m_fontName = WPXString(stringValue.c_str());
-
-		   WPD_DEBUG_MSG(("WordPerfect: stripping font name (final: %s)\n", m_fontName.cstr()));
-	   }
-   WPD_DEBUG_MSG(("WordPerfect: Read Font (primary family id: %i, family member id: %i, font type: %i, font source file type: %i font name length: %i, font name: %s)\n", (int) m_primaryFamilyId, (int) m_primaryFamilyMemberId, (int) m_fontType, (int) m_fontSourceFileType, (int) m_fontNameLength, m_fontName.cstr()));
-
+		WPD_DEBUG_MSG(("WordPerfect: stripping font name (original: %s)\n", m_fontName.cstr()));
+		std::string stringValue(m_fontName.cstr());
+		std::string::size_type pos;
+		for (unsigned k = 0; k < countElements(FONT_WEIGHT_STRINGS); k++)
+		{
+			if (!stringValue.empty())
+				while ((pos = stringValue.find(FONT_WEIGHT_STRINGS[k])) != std::string::npos)
+	  				stringValue.replace(pos, strlen(FONT_WEIGHT_STRINGS[k]),"");
+		}
+		// SPECIAL CASE: eliminate the -WP postfix (if it's there), which isn't spaced out from
+		// the rest of the font
+		if (!stringValue.empty())
+			while ((pos = stringValue.find(USELESS_WP_POSTFIX)) != std::string::npos)
+			stringValue.replace(pos, strlen(USELESS_WP_POSTFIX), "");
+			// also consume any whitespace at the end of the font.
+		if (!stringValue.empty())
+			while ((pos = stringValue.find("  ")) != std::string::npos)
+			stringValue.replace(pos, strlen("  "), " ");
+		if (!stringValue.empty())
+			while ((pos = stringValue.find(" ", stringValue.size() - 1)) != std::string::npos)
+			stringValue.replace(pos, strlen(" "), "");
+		if (!stringValue.empty())
+			while ((pos = stringValue.find("-", stringValue.size() - 1)) != std::string::npos)
+			stringValue.replace(pos, strlen("-"), "");
+	
+		m_fontName = WPXString(stringValue.c_str());
+			WPD_DEBUG_MSG(("WordPerfect: stripping font name (final: %s)\n", m_fontName.cstr()));
+	}
+	WPD_DEBUG_MSG(("WordPerfect: Read Font (primary family id: %i, family member id: %i, font type: %i, font source file type: %i font name length: %i, font name: %s)\n",
+		(int) m_primaryFamilyId, (int) m_primaryFamilyMemberId, (int) m_fontType, (int) m_fontSourceFileType, (int) m_fontNameLength, m_fontName.cstr()));
 }
