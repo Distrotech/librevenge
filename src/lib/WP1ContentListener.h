9fdffeb4999c1b84e9bbc4510470c4c9f463558b
Fridrich Strba <fridrich.strba@bluewin.ch>
2007-09-17 11:18:48 +0000

    simple conversion from mac font id to font-name for the most used Mac fonts

--- src/lib/WP1ContentListener.h
+++ src/lib/WP1ContentListener.h
@@ -60,6 +60,7 @@ public:
 	void insertNote(const WPXNoteType noteType, WP1SubDocument *subDocument);
 	void attributeChange(const bool isOn, const uint8_t attribute);
 	void fontPointSize(const uint8_t pointSize);
+	void fontId(const uint16_t fontId);
 	void marginReset(const uint16_t leftMargin, const uint16_t rightMargin);
 	void topMarginSet(const uint16_t /* topMargin */) {}
 	void bottomMarginSet(const uint16_t /* bottomMargin */) {}
---
+++ src/lib/WP1FontIdGroup.h
@@ -0,0 +1,42 @@
+/* libwpd
+ * Copyright (C) 2007Fridrich Strba (fridrich.strba@bluewin.ch)
+ *  
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * For further information visit http://libwpd.sourceforge.net
+ */
+
+/* "This product is not manufactured, approved, or supported by 
+ * Corel Corporation or Corel Corporation Limited."
+ */
+
+#ifndef WP1FONTIDGROUP_H
+#define WP1FONTIDGROUP_H
+
+#include "WP1FixedLengthGroup.h"
+
+class WP1FontIdGroup : public WP1FixedLengthGroup
+{
+public:
+	WP1FontIdGroup(WPXInputStream *input, uint8_t group);
+	~WP1FontIdGroup();	
+	void _readContents(WPXInputStream *input);
+	void parse(WP1Listener *listener);
+
+private:
+	uint16_t m_oldFontId, m_newFontId;
+};
+
+#endif /* WP1FONTIDGROUP_H */
