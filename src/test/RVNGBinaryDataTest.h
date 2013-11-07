d7b8016a2d89d1df3020a000626175e4fe42da1d
David Tardon <dtardon@redhat.com>
2013-11-07 10:35:18 +0100

    add skel. unit test for RVNGBinaryData

---
+++ src/test/RVNGBinaryDataTest.h
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/* librevenge
+ * Version: MPL 2.0 / LGPLv2.1+
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Alternatively, the contents of this file may be used under the terms
+ * of the GNU Lesser General Public License Version 2.1 or later
+ * (LGPLv2.1+), in which case the provisions of the LGPLv2.1+ are
+ * applicable instead of those above.
+ */
+
+#ifndef RVNGBINARYDATATEST_H
+#define RVNGBINARYDATATEST_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace test
+{
+
+class RVNGBinaryDataTest : public CPPUNIT_NS::TestFixture
+{
+    virtual void setUp();
+    virtual void tearDown();
+
+    CPPUNIT_TEST_SUITE(RVNGBinaryDataTest);
+    CPPUNIT_TEST_SUITE_END();
+};
+
+}
+
+#endif // RVNGBINARYDATATEST_H
+
+/* vim:set shiftwidth=4 softtabstop=4 noexpandtab: */
