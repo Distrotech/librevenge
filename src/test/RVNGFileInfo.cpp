ef58647b9f2ef6ba313feebad9df3a6617e5c536
David Tardon <dtardon@redhat.com>
2014-11-26 16:31:41 +0100

    add modelines

--- src/test/RVNGFileInfo.cpp
+++ src/test/RVNGFileInfo.cpp
@@ -1,3 +1,4 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
 
 #include <assert.h>
 #include <stdio.h>
@@ -40,3 +41,5 @@ int main(int argc, char *argv[])
 	}
 	return 0;
 }
+
+/* vim:set shiftwidth=4 softtabstop=4 noexpandtab: */
