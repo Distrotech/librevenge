28055b6289c59b3b43c790fa84f8ad437a171da6
David Tardon <dtardon@redhat.com>
2014-12-22 19:46:02 +0100

    get more info about structured stream's content

--- src/test/RVNGFileInfo.cpp
+++ src/test/RVNGFileInfo.cpp
@@ -5,6 +5,31 @@
 #include <boost/scoped_ptr.hpp>
 #include <librevenge-stream/librevenge-stream.h>
 
+namespace
+{
+
+static unsigned long getLength(librevenge::RVNGInputStream *const input)
+{
+	assert(input);
+
+	const long pos = input->tell();
+	if (0 > pos)
+		return 0;
+
+	long end = 0;
+
+	if (0 == input->seek(0, librevenge::RVNG_SEEK_END))
+		end = input->tell();
+	if ((0 != input->seek(pos, librevenge::RVNG_SEEK_SET)) || (0 > end))
+		return 0;
+
+	assert(pos <= end);
+
+	return static_cast<unsigned long>(end);
+}
+
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
@@ -27,17 +52,39 @@ int main(int argc, char *argv[])
 	{
 		const char *const name = input.subStreamName(i);
 		assert(input.existsSubStream(name));
-		printf("--> Stream %i: %s\n", i, name);
 
 		boost::scoped_ptr<librevenge::RVNGInputStream> subStream(input.getSubStreamById(i));
+		boost::scoped_ptr<librevenge::RVNGInputStream> namedSubStream(input.getSubStreamByName(name));
+		bool subStreamIsStructured = false;
+		bool namedSubStreamIsStructured = false;
+		unsigned long subStreamSize = 0;
+		unsigned long namedSubStreamSize = 0;
+
+		if (bool(subStream))
+		{
+			subStreamIsStructured = subStream->isStructured();
+			subStreamSize = getLength(subStream.get());
+		}
+
+		if (bool(namedSubStream))
+		{
+			namedSubStreamIsStructured = namedSubStream->isStructured();
+			namedSubStreamSize = getLength(namedSubStream.get());
+		}
+
+		if (subStreamIsStructured)
+			printf("--> Stream %i: %s\n", i, name);
+		else
+			printf("--> Stream %i: %s (size: %lu)\n", i, name, subStreamSize);
+
 		if (!subStream)
 			printf("Failed to get the substream by id\n");
-
-		boost::scoped_ptr<librevenge::RVNGInputStream> namedSubStream(input.getSubStreamByName(name));
 		if (!namedSubStream)
 			printf("Failed to get the substream by name\n");
 
 		assert((bool(subStream) && bool(namedSubStream)) || (!subStream && !namedSubStream));
+		assert(subStreamIsStructured == namedSubStreamIsStructured);
+		assert(subStreamSize == namedSubStreamSize);
 	}
 	return 0;
 }
