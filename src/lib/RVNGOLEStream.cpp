2115450745229343b373bdebd0922bd0b54fc6ad
David Tardon <dtardon@redhat.com>
2014-12-22 19:56:54 +0100

    check for minimal size of OLE2 too

--- src/lib/RVNGOLEStream.cpp
+++ src/lib/RVNGOLEStream.cpp
@@ -754,6 +754,8 @@ bool librevenge::Header::valid(const unsigned long fileSize)
 {
 	if (m_threshold != 4096) return false;
 	if ((fileSize % m_size_bbat) != 0) return false;
+	// there must be at least the header, one bat sector and one dirent sector in the file
+	if ((fileSize / m_size_bbat) < 3) return false;
 	if (m_num_bat == 0) return false;
 	if ((m_num_bat > 109) && (m_num_bat > (m_num_mbat * (m_size_bbat/4-1)) + 109)) return false;
 	if ((m_num_bat < 109) && (m_num_mbat != 0)) return false;
