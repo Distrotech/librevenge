e682c92276a48e460980349c13e5fe143a02b247
David Tardon <dtardon@redhat.com>
2015-08-28 15:23:48 +0200

    fix possible access to element of empty vector
    The data buffer is never empty, so it cannot be used to check if
    anything was decompressed.

--- src/lib/RVNGZipStream.cpp
+++ src/lib/RVNGZipStream.cpp
@@ -487,7 +487,7 @@ RVNGInputStream *RVNGZipStream::getSubstream(RVNGInputStream *input, const char 
 
 		(void)inflateEnd(&strm);
 
-		if (data.empty())
+		if (strm.total_out == 0)
 			return 0;
 		return new RVNGStringStream(&data[0], strm.total_out);
 	}
